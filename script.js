d85bb0370ddd12d3d5357b2ef82dfa3b2279f5fd
rajnandani987sharma-afk <rajnandani987sharma@gmail.com>
2026-03-30 23:41:42 +0530

    Add login function to handle user authentication

---
+++ script.js
@@ -0,0 +1,12 @@
+function login(){
+
+let name=document.getElementById("name").value
+
+localStorage.setItem("student",name)
+
+document.getElementById("login").style.display="none"
+document.getElementById("dashboard").style.display="block"
+
+document.getElementById("username").innerText=name
+
+}
