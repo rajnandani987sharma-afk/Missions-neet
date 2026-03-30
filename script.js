158d0032bd3d2b3e39499255e1cb863272b1931b
rajnandani987sharma-afk <rajnandani987sharma@gmail.com>
2026-03-31 00:33:30 +0530

    Implement mock test functionality in script.js

--- script.js
+++ script.js
@@ -15,3 +15,54 @@ function startTest(){
 alert("Mock Test coming soon!")
 
 }
+let current=0
+let score=0
+
+function startTest(){
+
+document.getElementById("dashboard").innerHTML=
+`
+
+<h2>Mission NEET Mock Test</h2><div id="question"></div><div id="options"></div><br><button onclick="nextQuestion()">Next</button>
+`
+
+showQuestion()
+
+}
+
+function showQuestion(){
+
+let q=questions[current]
+
+document.getElementById("question").innerHTML=
+"<h3>Q${current+1}. ${q.q}</h3>"
+
+document.getElementById("options").innerHTML=
+"<button onclick="checkAnswer('a')">${q.a}</button><br><br> <button onclick="checkAnswer('b')">${q.b}</button><br><br> <button onclick="checkAnswer('c')">${q.c}</button><br><br> <button onclick="checkAnswer('d')">${q.d}</button>"
+}
+
+function checkAnswer(ans){
+
+if(ans==questions[current].correct){
+score++
+}
+
+}
+
+function nextQuestion(){
+
+current++
+
+if(current<questions.length){
+
+showQuestion()
+
+}
+else{
+
+document.getElementById("dashboard").innerHTML=
+"<h2>Your Score: ${score} / ${questions.length}</h2>"
+
+}
+
+  }
