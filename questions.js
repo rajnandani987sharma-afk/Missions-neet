c2477050c710d2632fe2f5055e0d67d50c27bc92
rajnandani987sharma-afk <rajnandani987sharma@gmail.com>
2026-03-31 00:31:45 +0530

    Add quiz questions to questions.js
    Added a list of quiz questions with multiple choice answers.

--- questions.js
+++ questions.js
@@ -1,1 +1,42 @@
-
+const questions = [
+{
+q:"Which organelle is called powerhouse of the cell?",
+a:"Nucleus",
+b:"Mitochondria",
+c:"Ribosome",
+d:"Golgi body",
+correct:"b"
+},
+{
+q:"Unit of force is?",
+a:"Newton",
+b:"Joule",
+c:"Pascal",
+d:"Watt",
+correct:"a"
+},
+{
+q:"pH of pure water is?",
+a:"5",
+b:"6",
+c:"7",
+d:"8",
+correct:"c"
+},
+{
+q:"Which vitamin is produced in sunlight?",
+a:"Vitamin A",
+b:"Vitamin B",
+c:"Vitamin C",
+d:"Vitamin D",
+correct:"d"
+},
+{
+q:"Human heart has how many chambers?",
+a:"2",
+b:"3",
+c:"4",
+d:"5",
+correct:"c"
+}
+];
