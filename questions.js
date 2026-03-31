97f701d31b57fb21ad4e5e477c0074190d44a09e
rajnandani987sharma-afk <rajnandani987sharma@gmail.com>
2026-03-31 11:14:36 +0530

    Refactor questions structure and add new entries

--- questions.js
+++ questions.js
@@ -1,17 +1,8 @@
 const questions=[
 
 {
-subject:"Biology",
-q:"Powerhouse of the cell?",
-a:"Nucleus",
-b:"Mitochondria",
-c:"Ribosome",
-d:"Golgi body",
-correct:"b"
-},
-
-{
-subject:"Physics",
+subject:"physics",
+chapter:"Motion",
 q:"Unit of force?",
 a:"Newton",
 b:"Joule",
@@ -21,47 +12,8 @@ correct:"a"
 },
 
 {
-subject:"Chemistry",
-q:"pH of pure water?",
-a:"5",
-b:"6",
-c:"7",
-d:"8",
-correct:"c"
-},
-
-{
-subject:"Biology",
-q:"Human heart chambers?",
-a:"2",
-b:"3",
-c:"4",
-d:"5",
-correct:"c"
-},
-
-{
-subject:"Biology",
-q:"Sunlight vitamin?",
-a:"A",
-b:"B",
-c:"C",
-d:"D",
-correct:"d"
-},
-
-{
-subject:"Physics",
-q:"Speed of light?",
-a:"3×10^8",
-b:"3×10^6",
-c:"3×10^5",
-d:"3×10^4",
-correct:"a"
-},
-
-{
-subject:"Chemistry",
+subject:"chemistry",
+chapter:"Atomic Structure",
 q:"Atomic number of Oxygen?",
 a:"6",
 b:"7",
@@ -71,13 +23,25 @@ correct:"c"
 },
 
 {
-subject:"Biology",
-q:"Largest organ of human body?",
-a:"Heart",
-b:"Skin",
-c:"Liver",
-d:"Brain",
+subject:"botany",
+chapter:"Cell",
+q:"Powerhouse of cell?",
+a:"Nucleus",
+b:"Mitochondria",
+c:"Ribosome",
+d:"Golgi body",
 correct:"b"
+},
+
+{
+subject:"zoology",
+chapter:"Human Body",
+q:"Human heart chambers?",
+a:"2",
+b:"3",
+c:"4",
+d:"5",
+correct:"c"
 }
 
 ]
